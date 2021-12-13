fa6599e6bb9ff780d251401e1c50721a9c0da4b1
podyankee <podyankee@gmail.com>
2021-12-13 07:06:27 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import slideMenu from './partials/menu.js'
+
+slideMenu({
+	openBtn: '.header__burger-btn',
+	menu: '.navigation',
+	classActiveMenu: 'navigation_active',
+	closeTrigger: '.navigation__link, .navigation__close'
+});
